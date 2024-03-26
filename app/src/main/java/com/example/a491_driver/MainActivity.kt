8e011922371940cc05ed6399850622a54e987515
Peter <pja4@njit.edu>
2024-03-26 13:11:07 -0400

    initialized repo and application

---
+++ app/src/main/java/com/example/a491_driver/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a491_driver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val API_KEY = "2K3xFgG1BbcwRzw2gQSuFVPitxuh75dz"
+
+
+//        val mapWebView: WebView = findViewById(R.id.mapWebView)
+//        mapWebView.settings.javaScriptEnabled = true
+//        mapWebView.loadUrl("file:///android_asset/map.html")
+    }
+}
