35d00fbc506242c165e941cccca8d52461496760
Ryan McCusker <ryan@ryan-laptop.mynetworksettings.com>
2024-04-05 12:20:58 -0400

    apiservice and retrofitclient

---
+++ app/src/main/java/com/example/a491_driver/APIService.kt
@@ -0,0 +1,10 @@
+package com.example.a491_driver
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+interface APIService {
+
+}
